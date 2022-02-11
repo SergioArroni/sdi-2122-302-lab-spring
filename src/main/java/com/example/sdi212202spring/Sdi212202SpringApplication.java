b7e167d8eefa5443e3e8b733bf3cb8ea7bbce931
Sergio Arroni <uo276341@uniovi.es>
2022-02-11 09:46:01 +0100

    SDI-2122-302-2.1-Create Spring project.

---
+++ src/main/java/com/example/sdi212202spring/Sdi212202SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdi212202spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi212202SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi212202SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sdi212202spring/Sdi212202SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdi212202spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi212202SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
