ca5d20743e4cfd23810b8d564dd04e5275716401
Sergio Arroni <uo276341@uniovi.es>
2022-02-13 00:38:58 +0100

    SDI-2021-302-2.4-Modelo, acceso a datos simple.

---
+++ src/main/java/com/example/sdi212202spring/repository/MarksRepository.java
@@ -0,0 +1,8 @@
+package com.example.sdi212202spring.repository;
+
+import com.example.sdi212202spring.entities.Mark;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarksRepository extends CrudRepository<Mark, Long> {
+
+}
