c3898ab19509248e62ad87879a9b10d18fc7a05e
Sergio Arroni <uo276341@uniovi.es>
2022-03-05 14:15:06 +0100

    SDI-2122-302-5.7-Casos Uso VistaPrivada.

---
+++ src/test/java/com/example/sdi212202spring/pageobjects/PO_PrivateView.java
@@ -0,0 +1,24 @@
+package com.example.sdi212202spring.pageobjects;
+
+import com.example.sdi212202spring.util.SeleniumUtils;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.*;
+
+public class PO_PrivateView extends PO_NavView {
+
+    static public void fillFormAddMark(WebDriver driver, int userOrder, String descriptionp, String scorep) {
+        //Esperamos 5 segundo a que carge el DOM porque en algunos equipos falla
+        SeleniumUtils.waitSeconds(driver, 5); //Seleccionamos el alumnos userOrder
+        new Select(driver.findElement(By.id("user"))).selectByIndex(userOrder); //Rellenemos el campo de descripción
+        WebElement description = driver.findElement(By.name("description"));
+        description.clear();
+        description.sendKeys(descriptionp);
+        WebElement score = driver.findElement(By.name("score"));
+        score.click();
+        score.clear();
+        score.sendKeys(scorep);
+        By boton = By.className("btn");
+        driver.findElement(boton).click();
+    }
+
+}
